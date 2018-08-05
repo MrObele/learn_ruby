41b082a10faff59fad7caf74ac9a08bd82af9964
mrobele <obeledavid@yahoo.com>
2018-08-05 12:41:33 +0100

    Completed the ftoc function with all casese of conversion from fahrenheit to celsius passed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(number_in_fahrenheit)
+	number_in_celsius = ((number_in_fahrenheit.round - 32) / (9/5.to_f)).to_i
+	number_in_celsius
+end
+
+def ctof(number_in_celsius)
+	number_in_fahrenheit = ((9/5.to_f * number_in_celsius) + 32).to_i
+	number_in_fahrenheit
+end
