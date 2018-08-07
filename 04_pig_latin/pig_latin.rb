f69b27e848b9fa367eae7e222cbf5c29c17f861c
mrobele <obeledavid@yahoo.com>
2018-08-07 08:54:29 +0100

    Added solution for 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
 #write your code here
+def translate(word_to_translate)
+	
+ word_to_translate.split(" ").map{ |word|
+   match_vowels = word.match(/[aeio]/)
+	word unless match_vowels
+	match_vowels.to_s<<match_vowels.post_match<<match_vowels.pre_match<<'ay'}.join(" ")
+end
+
