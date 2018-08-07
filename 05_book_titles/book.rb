1f473aeb33a5421369187a814bafd0f841a99e4e
mrobele <obeledavid@yahoo.com>
2018-08-07 22:34:38 +0100

    Added 05_book_titles solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(new_title)
+   exempted_words = ['a','an', 'and', 'the', 'in', 'of']
+   capitalized = new_title.split(" ")
+  capitalized[0].capitalize!
+  
+  @title = (capitalized.each do|word|
+    (exempted_words.include? word)? word: word.capitalize!
+     end).join(" ")  	
+  end
+
 end
