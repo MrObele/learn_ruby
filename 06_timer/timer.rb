8ce60b7fb87e125ed0f3d24a8f930f58285bb20b
mrobele <obeledavid@yahoo.com>
2018-08-07 23:13:22 +0100

    Updated 06_timer solution to a more optimized one

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -2,13 +2,20 @@ class Timer
   attr_accessor :seconds
 
   def initialize
-    @seconds = 0
+    @seconds = 00
   end
 
+def padded(x)
+    if x.to_s.length == 1
+        return "0"+x.to_s
+    else
+        return x.to_s
+    end
+end
   def time_string
     hours = @seconds/3600
     remainder = @seconds%3600
-    sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+   "#{padded(hours)}:#{padded(remainder/60)}:#{padded(remainder%60)}"
   end
   
 end
